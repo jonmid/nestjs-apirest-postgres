859922d55312fa8202d454b043846085104234eb
JonMid <10jonmid@gmail.com>
2021-07-07 11:24:29 -0500

    [update] Config file enviroment variables data base

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,31 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { Client } from 'pg';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+import config from '../config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
-const client = new Client({
-  user: 'root',
-  host: 'localhost',
-  database: 'my_db',
-  password: '123456',
-  port: 5432,
-});
-
-client.connect();
-
 @Global()
 @Module({
+  // imports: [
+  //   TypeOrmModule.forRootAsync({
+  //     inject: [config.KEY],
+  //     useFactory: (configService: ConfigType<typeof config>) => {
+  //       const { user, host, dbName, password, port } = configService.postgres;
+  //       return {
+  //         type: 'postgres',
+  //         host,
+  //         port,
+  //         username: user,
+  //         password,
+  //         database: dbName,
+  //       };
+  //     },
+  //   }),
+  // ],
   providers: [
     {
       provide: 'API_KEY',
@@ -23,9 +33,22 @@ client.connect();
     },
     {
       provide: 'PG',
-      useValue: client,
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+        const client = new Client({
+          user,
+          host,
+          database: dbName,
+          password,
+          port,
+        });
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
     },
   ],
+  // exports: ['API_KEY', 'PG', TypeOrmModule],
   exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
