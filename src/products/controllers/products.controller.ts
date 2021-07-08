4ac389822096ecaa1631f50ef7f44efb0f7f4fb2
JonMid <10jonmid@gmail.com>
2021-07-07 20:43:38 -0500

    [update] methods create, update, delete entity products with TypeORM

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,21 +1,18 @@
 import {
   Controller,
   Get,
-  // Query,
   Param,
-  // Post,
-  // Body,
-  // Put,
-  // Delete,
+  Post,
+  Body,
+  Put,
+  Delete,
   HttpStatus,
   HttpCode,
-  // ParseIntPipe,
+  ParseIntPipe,
 } from '@nestjs/common';
-// import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-import { ParseIntPipe } from '../../common/parse-int.pipe';
-// import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
+import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
 import { ProductsService } from './../services/products.service';
 
 @ApiTags('products')
@@ -40,18 +37,18 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
-  // @Post()
-  // create(@Body() payload: CreateProductDto) {
-  //   return this.productsService.create(payload);
-  // }
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
 
-  // @Put(':id')
-  // update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
-  //   return this.productsService.update(+id, payload);
-  // }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: UpdateProductDto) {
+    return this.productsService.update(id, payload);
+  }
 
-  // @Delete(':id')
-  // delete(@Param('id') id: string) {
-  //   return this.productsService.remove(+id);
-  // }
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productsService.remove(id);
+  }
 }
