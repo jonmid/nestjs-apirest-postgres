e0c0448761a85065daa150070a9bb185d5432d13
JonMid <10jonmid@gmail.com>
2021-07-09 17:38:01 -0500

    [add] Config serializar in entity return data

--- src/users/entities/order-product.entity.ts
+++ src/users/entities/order-product.entity.ts
@@ -6,6 +6,7 @@ import {
   Column,
   ManyToOne,
 } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 import { Product } from '../../products/entities/product.entity';
 import { Order } from './order.entity';
@@ -18,12 +19,14 @@ export class OrderProduct {
   @Column({ type: 'int' })
   quantity: number;
 
+  @Exclude()
   @CreateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -6,6 +6,8 @@ import {
   Entity,
   OneToMany,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
+
 import { Customer } from './customer.entity';
 import { OrderProduct } from './order-product.entity';
 
@@ -29,6 +31,34 @@ export class Order {
   @ManyToOne(() => Customer, (customer) => customer.orders)
   customer: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderProduct, (item) => item.order)
   items: OrderProduct[];
+
+  @Expose()
+  get products() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+          order_product_id: item.id,
+        }));
+    }
+    return [];
+  }
+
+  @Expose()
+  get total() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .reduce((total, item) => {
+          const totalItem = item.product.price * item.quantity;
+          return total + totalItem;
+        }, 0);
+    }
+    return 0;
+  }
 }
